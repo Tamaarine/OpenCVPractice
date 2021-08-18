843f3b2a089b9cb3df41f7ca1e3d2ee057a5cad4
RICKY LU <rilu@cs.stonybrook.edu>
2021-08-18 13:46:33 -0400

    The most basic function. Canny edges, blurring, eroding, resizing, and cropping edges, and gray scale

---
+++ basic.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+
+img = cv.imread('img/cat.jpg')
+
+cv.imshow('Cat', img)
+
+# Convert to greyscale
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+
+# Blur image
+blur = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+
+# Edge Cascade
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+
+# Dilating the image. Basically highlighting making the edges thicker
+dilated = cv.dilate(canny, (7, 7), iterations=3)
+cv.imshow('Dilated', dilated)
+
+
+# Eroding, basically it dim the edges, opposite of dilated
+eroded = cv.erode(dilated, (3, 3), iterations=1)
+cv.imshow('Eroded', eroded)
+
+
+# Resize 
+resized = cv.resize(img, (100, 100), interpolation=cv.INTER_AREA)
+cv.imshow('Resized', resized)
+
+
+# Cropping
+cropped = img[50: 200, 200:400]
+cv.imshow('Cropped', cropped)
+
+cv.waitKey(0)
